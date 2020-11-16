185febd020556e6467ecba0f70628173bfb4a37c
Adewale Sanusi <adewalesanusi.a@gmail.com>
2020-11-16 15:23:46 +0000

    Made the questions readable

--- main.py
+++ main.py
@@ -101,7 +101,10 @@ print(lucky_number("Cameron"))
 
 # Question 8
 
-# Flesh out the body of the print_seconds function so that it prints the total amount of seconds given the hours, minutes, and seconds function parameters. Remember that there are 3600 seconds in an hour and 60 seconds in a minute.
+# Flesh out the body of the print_seconds function 
+# so that it prints the total amount of seconds
+# given the hours, minutes, and seconds function parameters.
+# Remember that there are 3600 seconds in an hour and 60 seconds in a minute.
 
 def print_seconds(hours, minutes, seconds):
     print(hours * 3600 + minutes * 60 + seconds)
@@ -112,7 +115,11 @@ print_seconds(1,2,3)
 
 # Question 9
 
-#Use the get_seconds function to work out the amount of seconds in 2 hours and 30 minutes, then add this number to the amount of seconds in 45 minutes and 15 seconds. Then print the result.
+# Use the get_seconds function to work out the amount
+# of seconds in 2 hours and 30 minutes, 
+# then add this number to the amount of
+# seconds in 45 minutes and 15 seconds. 
+# Then print the result.
 
 def get_seconds(hours, minutes, seconds):
   return 3600*hours + 60*minutes + seconds
